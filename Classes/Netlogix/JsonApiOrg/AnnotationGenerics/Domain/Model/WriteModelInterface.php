afde1dafc73c07a62304415a8cff53a316771f4c
Stephan Schuler <stephan.schuler@netlogix.de>
2016-05-17 18:35:37 +0200

    [TASK] Add interface for generic read and write models

---
+++ Classes/Netlogix/JsonApiOrg/AnnotationGenerics/Domain/Model/WriteModelInterface.php
@@ -0,0 +1,18 @@
+<?php
+namespace Netlogix\JsonApiOrg\AnnotationGenerics\Domain\Model;
+
+/*
+ * This file is part of the Netlogix.JsonApiOrg.AnnotationGenerics package.
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+
+interface WriteModelInterface extends GenericModelInterface
+{
+    public function execute();
+
+}
